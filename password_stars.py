8fafef1030f47a95d88c39d40806ca3426a8c8ea
Ctoze <clayton.tozer@my.jcu.edu.au>
2022-09-25 21:07:26 +1000

    Refactor password and temperatures program to use functions

--- password_stars.py
+++ password_stars.py
@@ -1,15 +1,26 @@
-print("Your password needs to be a minimum of 8 characters long")
-password = input("ENTER PASSWORD: ")
-res = any(ele.isupper() for ele in password)
-count = 0
+def main():
+    password = get_password()
+    res = any(ele.isupper() for ele in password)
+    count = 0
 
-if len(password) < 8:
-    password = input("Password invalid, Please enter Password: ")
-if res == False:
-    password = input("Password invalid, Please enter Password: ")
+    if len(password) < 8:
+        password = input("Password invalid, Please enter Password: ")
+    if res == False:
+        password = input("Password invalid, Please enter Password: ")
 
-for i in range(0, len(password)):
-    if(password[i] != ' '):
-        count = count + 1
+    for i in range(0, len(password)):
+        if (password[i] != ' '):
+            count = count + 1
 
-print('*' * count)
+    print('*' * count)
+
+
+def get_password():
+    print("Your password needs to be a minimum of 8 characters long")
+    password = input("ENTER PASSWORD: ")
+    return password
+
+
+main()
+
+
